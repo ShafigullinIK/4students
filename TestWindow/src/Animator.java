d99a58c65ab85b98541aa16afc2fbd2779f1d562
ShafigullinIK <44900148+ShafigullinIK@users.noreply.github.com>
2019-11-18 10:57:48 +0300

    Window (11/18/2019)

--- TestWindow/src/Animator.java
+++ TestWindow/src/Animator.java
@@ -1,19 +1,40 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 public class Animator {
 
-    Graphics2D graphics;
+    Graphics graphics;
+    Graphics screenGraphics;
 
-    int pressedX;
-    int pressedY;
+    final int CENTER_X = 600;
+    final int CENTER_Y = 400;
+    int CELL_SIZE = 100;
+    int serifSize = 10;
+
+    int currentX;
+    int currentY;
+    BufferedImage img;
+
 
     public Animator(Graphics graphics) {
-        this.graphics = (Graphics2D) graphics;
-        this.graphics.setRenderingHint(
-                RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON );
+        screenGraphics = graphics;
+        img = new BufferedImage(1200, 800,
+                BufferedImage.TYPE_3BYTE_BGR);
+        this.graphics = img.getGraphics();
+    }
 
+    public void changeCellSize(int a) {
+        resetImg();
+        if (a > 0) {
+            CELL_SIZE += 5;
+        } else {
+            if (CELL_SIZE > 7) {
+                CELL_SIZE -= 5;
+            }
+        }
+        drawAxis();
+        drawToScreen();
     }
 
     public void drawSquare(int x, int y) {
@@ -24,8 +45,74 @@ public class Animator {
 //        graphics.drawLine();
     }
 
-    public void drawLine(int x, int y) {
+    public void setXandY(int x, int y) {
+        resetImg();
+        currentX = x;
+        currentY = y;
+        double newX = coorTransX(x);
+        double newY = coorTransY(y);
+        graphics.fillOval(x - 5, y - 5, 10, 10);
+        graphics.drawString("x = " + newX +
+                "   y = " + newY, 800, 600);
+        drawAxis();
+        drawToScreen();
+    }
 
-        graphics.drawLine(pressedX, pressedY, x, y);
+    public void resetImg() {
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, img.getWidth(), img.getHeight());
+        graphics.setColor(Color.BLACK);
+    }
+
+    public double coorTransX(int x) {
+        double newX = (x - CENTER_X) * 1.0 / CELL_SIZE;
+        return newX;
+    }
+
+    public double coorTransY(int y) {
+        double newY = (CENTER_Y - y) * 1.0 / CELL_SIZE;
+        return newY;
+    }
+
+    public void drawAxis() {
+        drawX();
+        drawY();
+    }
+
+    public void drawX() {
+        graphics.drawLine(50, CENTER_Y,
+                2 * CENTER_X - 50, CENTER_Y);
+        graphics.drawLine(2 * CENTER_X - 80,
+                CENTER_Y - 15, 2 * CENTER_X - 50, CENTER_Y);
+        graphics.drawLine(2 * CENTER_X - 80, CENTER_Y + 15,
+                2 * CENTER_X - 50, CENTER_Y);
+        int countOfSerifs = (CENTER_X - 100) / CELL_SIZE;
+        for (int i = 0; i < countOfSerifs; i++) {
+            graphics.drawLine(CENTER_X + i * CELL_SIZE,
+                    CENTER_Y - serifSize,
+                    CENTER_X + i * CELL_SIZE,
+                    CENTER_Y + serifSize);
+            graphics.drawLine(CENTER_X - i * CELL_SIZE,
+                    CENTER_Y - serifSize,
+                    CENTER_X - i * CELL_SIZE, CENTER_Y + serifSize);
+        }
+    }
+
+    public void drawY() {
+        graphics.drawLine(600, 50, 600, 750);
+        graphics.drawLine(585, 80, 600, 50);
+        graphics.drawLine(615, 80, 600, 50);
+        int countOfSerifs = (CENTER_Y - 100) / CELL_SIZE;
+        for (int i = 0; i < countOfSerifs; i++) {
+            graphics.drawLine(CENTER_X - serifSize, CENTER_Y + i * CELL_SIZE,
+                    CENTER_X + serifSize, CENTER_Y + i * CELL_SIZE);
+            graphics.drawLine(CENTER_X - serifSize, CENTER_Y - i * CELL_SIZE,
+                    CENTER_X + serifSize, CENTER_Y - i * CELL_SIZE);
+        }
+    }
+
+    public void drawToScreen() {
+        screenGraphics.drawImage(img, 0, 0,
+                img.getWidth(), img.getHeight(), null);
     }
 }
--- TestWindow/src/Window.java
+++ TestWindow/src/Window.java
@@ -1,8 +1,5 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.util.Random;
 
 public class Window extends JFrame {
@@ -11,67 +8,18 @@ public class Window extends JFrame {
 
 //        this.setSize(600, 400);
         this.setBounds(350,
-                350, 600, 400);
+                150, 1200, 800);
         this.setDefaultCloseOperation(Window.EXIT_ON_CLOSE);
-//        this.setResizable(false);
+        this.setResizable(false);
         this.setLayout(null);
-        JButton button = new JButton();
-        button.setBounds(this.getWidth()/2,
-                this.getHeight()/2, 150, 75);
-        button.setText("Кнопочка");
-        this.add(button);
-        JTextField tf = new JTextField();
-        tf.setBounds(10, 10, 200, 50);
-        this.add(tf);
-        JTextField tf2 = new JTextField();
-        tf2.setBounds(250, 10, 200, 50);
-        this.add(tf2);
+
         this.setVisible(true);
 
         Animator animator = new Animator(this.getGraphics());
-
         this.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                int x = e.getX();
-                int y = e.getY();
-                animator.drawSquare(x,y);
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                animator.pressedX = e.getX();
-                animator.pressedY = e.getY();
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                animator.drawLine(e.getX(), e.getY());
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                button.setText(button.getText() + "!");
-
-            }
-        });
-
-        button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-
+                animator.setXandY(e.getX(), e.getY());
             }
 
             @Override
@@ -86,16 +34,20 @@ public class Window extends JFrame {
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                Random r = new Random();
-                int newX = r.nextInt(400);
-                int newY = r.nextInt(350);
-                button.setBounds(newX, newY,
-                        button.getWidth(), button.getHeight());
+
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
 
+            }
+        });
+
+        this.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                System.out.println(e.getWheelRotation());
+                animator.changeCellSize(e.getWheelRotation());
             }
         });
 
